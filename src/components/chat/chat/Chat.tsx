9007bd0ef260d75608b14c5336ab45947e136313
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-12 13:44:14 -0600

    feat: create the chat layout

--- src/components/chat/chat/Chat.tsx
+++ src/components/chat/chat/Chat.tsx
@@ -1,7 +1,15 @@
 export const Chat = () => {
   return (
-    <section className='[grid-area:chat] bg-secundary rounded-2xl w-full p-5'>
-      <h1>Hola mundo</h1>
+    <section className='[grid-area:chat] flex flex-col gap-5 min-h-[calc(100vh-40px)]'>
+      <main className='bg-secundary rounded-2xl p-5 h-full'>
+        <h1>Hola mundo</h1>
+      </main>
+
+      <input
+        type='text'
+        placeholder='Mensaje...'
+        className='w-full bg-light rounded-2xl h-16 px-5'
+      />
     </section>
-  )
-}
+  );
+};
--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,9 +1,9 @@
-import { Player } from '@/components/ui'
+import { Player } from '@/components/ui';
 export const Sidebar = () => {
   return (
-    <aside className='[grid-area:sidebar] justify-items-center bg-secundary md:w-96 rounded-2xl p-5 scroll-smooth overflow-y-auto'>
+    <aside className='[grid-area:sidebar] justify-items-center bg-secundary md:w-96 rounded-2xl p-5 scroll-smooth overflow-y-auto min-h-[90vh] md:min-h-0'>
       <Player />
       <hr className='w-full border border-light' />
     </aside>
-  )
-}
+  );
+};
