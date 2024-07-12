95a4a0a24d8f9cf9a713e00d6be10c1b99a1568d
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-12 14:06:16 -0600

    feat: create the styles for the messages

--- src/components/chat/chat/Chat.tsx
+++ src/components/chat/chat/Chat.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image';
+
 export const Chat = () => {
   return (
     <section className='[grid-area:chat] flex flex-col gap-5 min-h-[calc(100vh-40px)]'>
       <main className='bg-secundary rounded-2xl p-5 h-full'>
-        <h1>Hola mundo</h1>
+        <div className='message-container justify-end'>
+          <Image
+            src='/img/thanatos.png'
+            width={50}
+            height={50}
+            alt='Thanatos Avatar'
+            className='message-avatar order-1'
+          />
+
+          <p className='message-text'>
+            Hola, soy tu asistente de meditación. Mi nombre es Thanatos.
+          </p>
+        </div>
       </main>
 
       <input
