9aacd4dcd7c757dce3e87c55bdc49f52e3bd1b47
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-12 22:23:09 -0600

    feat(chat): add auto scroll end after submitting messages

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,33 +1,47 @@
-'use client'
+'use client';
 
-import { Message as MessageType, useChat } from 'ai/react'
-import { Message } from './message/Message'
+import { Message as MessageType, useChat } from 'ai/react';
+import { Message } from './message/Message';
+import { useEffect, useRef } from 'react';
 
 const initialMessages: MessageType[] = [
   {
     id: '12345678',
     content:
       '¡Hola! Soy un asistente de meditación. ¿Estás interesado en realizar una sesión de meditación guiada hoy?',
-    role: 'assistant'
-  }
-]
+    role: 'assistant',
+  },
+];
 
 export const Chat = () => {
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
   const { messages, input, handleInputChange, handleSubmit } = useChat({
-    initialMessages
-  })
+    initialMessages,
+  });
+
+  useEffect(() => {
+    if (!messagesContainerRef.current) return;
+
+    const { scrollHeight } = messagesContainerRef.current;
+
+    messagesContainerRef.current.scrollTop = scrollHeight;
+  }, [messages]);
 
   return (
     <section className='[grid-area:chat] flex flex-col gap-5 min-h-[calc(100vh-40px)]'>
-      <main className='bg-secundary rounded-2xl p-5 h-full overflow-y-scroll no-scrollbar flex flex-col gap-10'>
+      <main
+        className='bg-secundary rounded-2xl p-5 h-full overflow-y-scroll no-scrollbar flex flex-col gap-10'
+        ref={messagesContainerRef}
+      >
         {messages.map((message) => {
           return (
             <Message
               key={message.id}
-              role={message.role as 'user' | 'assistant'}>
+              role={message.role as 'user' | 'assistant'}
+            >
               {message.content}
             </Message>
-          )
+          );
         })}
       </main>
 
@@ -41,5 +55,5 @@ export const Chat = () => {
         />
       </form>
     </section>
-  )
-}
+  );
+};
