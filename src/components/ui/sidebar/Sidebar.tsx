8c6f4a358662a6bb365dc9f9b9466e1f5e35b890
Eduardo Porto <A01027893@tec.mx>
2024-07-12 18:47:59 -0600

    feat: music player

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
-import { Player } from '@/components/ui';
+//import { Player } from '@/components/ui';
+import Player from '../player/Player'
+
 export const Sidebar = () => {
   return (
     <aside className='[grid-area:sidebar] justify-items-center bg-secundary md:w-96 rounded-2xl p-5 scroll-smooth overflow-y-auto min-h-[90vh] md:min-h-0'>
