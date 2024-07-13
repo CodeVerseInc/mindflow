2eaeec6104c477d4d12170630e82c42d5b762614
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-12 22:25:07 -0600

    feat(chat): add smooth scroll to the messages container

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -30,7 +30,7 @@ export const Chat = () => {
   return (
     <section className='[grid-area:chat] flex flex-col gap-5 min-h-[calc(100vh-40px)]'>
       <main
-        className='bg-secundary rounded-2xl p-5 h-full overflow-y-scroll no-scrollbar flex flex-col gap-10'
+        className='bg-secundary rounded-2xl p-5 h-full overflow-y-scroll no-scrollbar flex flex-col gap-10 scroll-smooth'
         ref={messagesContainerRef}
       >
         {messages.map((message) => {
