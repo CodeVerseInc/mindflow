3aff9efa9575a56885d5d80aaae8b20f2acb0a99
LizandroBackend <codeversebackend@gmail.com>
2024-07-13 10:34:02 -0600

    ci: Add seed books, url_books provitional

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -18,6 +18,25 @@ export async function GET(request: Request) {
             { url_song: 'https://res.cloudinary.com/dlklqucye/video/upload/v1720841880/mindflow/songs/9_urebxg.mp3' },
             { url_song: 'https://res.cloudinary.com/dlklqucye/video/upload/v1720841870/mindflow/songs/10_rlsodh.mp3' },
         ]
+    })  
+     
+    //? This seed is provisional for url_book undefined
+     
+    await prisma.books.deleteMany() //! Deelete all books  
+     
+    await prisma.books.createMany({ 
+        data: [ 
+            {name_book: 'Habitos Atómicos', url_book: 'https://drive.google.com/file/d/1A1UlLuvmuXP1RKGJeSHnmGFa3bypqSPW/view?usp=sharing' }, 
+            {name_book: '48 leyes del poder', url_book: 'https://drive.google.com/file/d/17s9nUGQg4QUbAyT1Z2Km8qCALihzlpw2/view?usp=sharing' },
+            {name_book: 'Tus zonas erroneas', url_book: 'https://drive.google.com/file/d/1yh20CWcHgGW52_O8GitCn17zeeyPmcUP/view?usp=sharing' },
+            {name_book: 'El club de las 5 de la mañana', url_book: 'https://drive.google.com/file/d/1WPqbVDlG4KP6CCPfJ4whYBNYdpHdHsk6/view?usp=sharing' },
+            {name_book: 'Espabila de una p*ta vez', url_book: 'https://drive.google.com/file/d/1sVmz_uwQoVG0wJLyuiB7AX_Ym8FhRGd7/view?usp=sharing' },
+            {name_book: 'Meditaciones', url_book: 'https://drive.google.com/file/d/1tbdvfyNDgKxvaNPUd_Ao0JYzYj0C8ccn/view?usp=sharing' },
+            {name_book: 'La vida son 2 p*tos días', url_book: 'https://drive.google.com/file/d/18bYlEWSPxZfhu6OU77eJnIdDUckwOkDO/view?usp=sharing' },
+            {name_book: 'Quien se ha llevado mi queso', url_book: 'https://drive.google.com/file/d/1XMobjvkwLCGJVW-y_lI7eQ0KhOiLwIfr/view?usp=sharing' },
+            {name_book: 'El monge que vendió su ferrari', url_book: 'https://drive.google.com/file/d/1J4FL_ObfLmi8--2cZ8I1ncm_M0215EVp/view?usp=sharing' },
+            {name_book: 'Manual de vida', url_book: 'https://drive.google.com/file/d/1vOizDccZ0B8k4FCz1sdkiRaNH2S3VD24/view?usp=sharing' },
+        ]
     })
      
 
