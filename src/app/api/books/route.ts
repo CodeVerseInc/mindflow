266c6716ea2ab2f66233b827ee8474f79b717709
LizandroBackend <codeversebackend@gmail.com>
2024-07-15 10:56:27 -0600

    ci: Add function take and skip to the route books

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,8 +1,21 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server"; 
  
-export async function GET(request: Request) { 
-    const books = await prisma.books.findMany(); 
+export async function GET(request: Request) {  
+     
+    const { searchParams } = new URL(request.url) 
+    const take = Number(searchParams.get('take') ?? '10'); 
+    const skip = Number(searchParams.get('skip') ?? '0'); 
+     
+    if (isNaN(take)) { 
+        return NextResponse.json({ message: 'Take tiene que ser un número'}, { status: 400 });
+    } 
+     
+    if (isNaN(skip)) { 
+        return NextResponse.json({ message: 'Skip tiene que ser un número'}, { status: 400 });
+    } 
+
+    const books = await prisma.books.findMany({ take, skip }); 
      
     return NextResponse.json(books);
 }
