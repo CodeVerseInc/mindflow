c31852e88b862cb4ccf7f857ef558bc59aacd982
LizandroBackend <codeversebackend@gmail.com>
2024-07-15 10:53:22 -0600

    ci: Add function Take and Skip to the route songs, lack route books

--- src/app/api/songs/route.ts
+++ src/app/api/songs/route.ts
@@ -1,8 +1,21 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server"; 
  
-export async function GET(request: Request) { 
-    const songs = await prisma.songs.findMany(); 
+export async function GET(request: Request) {  
+     
+    const { searchParams } = new URL(request.url) 
+    const take = Number(searchParams.get('take') ?? '10');  
+    const skip = Number(searchParams.get('skip') ?? '0'); 
+ 
+    if (isNaN(take)) { 
+        return NextResponse.json({ message: 'Take tiene que ser un número'}, { status: 400 });
+    }
+     
+    if (isNaN(skip)) { 
+        return NextResponse.json({ message: 'Skip tiene que ser un número'}, { status: 400 });
+    } 
+
+    const songs = await prisma.songs.findMany({ take, skip }); 
      
     return NextResponse.json(songs);
 }
