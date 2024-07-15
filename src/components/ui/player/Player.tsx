a95c22ade7773aa5accca0232b48505790b0af4c
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-07-14 20:49:01 -0600

    feat: add progress bar

--- src/components/ui/player/Player.tsx
+++ src/components/ui/player/Player.tsx
@@ -7,14 +7,18 @@ import {
   IconPlayerSkipForward
 } from '@tabler/icons-react'
 import { ButtonPlayer } from './button-player/ButtonPlayer'
+import styles from './audioplayer.module.css'
 
 export const Player = () => {
   const [isPlaying, setIsPlaying] = useState(false)
   const [currentTime, setCurrentTime] = useState(0)
-  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const audioPlayer = useRef<HTMLAudioElement>(null)
+  const progressBar = useRef<HTMLInputElement>(null)
+  const animationRef = useRef<number>()
 
   useEffect(() => {
-    const audio = audioRef.current
+    const audio = audioPlayer.current
     if (audio) {
       const updateTime = () => setCurrentTime(audio.currentTime)
       audio.addEventListener('timeupdate', updateTime)
@@ -25,43 +29,75 @@ export const Player = () => {
   }, [])
 
   const togglePlay = () => {
-    if (audioRef.current) {
+    if (audioPlayer.current) {
       if (isPlaying) {
-        console.log('pause')
-        audioRef.current.pause()
+        audioPlayer.current.pause()
       } else {
-        console.log('play')
-        audioRef.current.play()
+        audioPlayer.current.play()
       }
       setIsPlaying(!isPlaying)
     }
   }
 
+  const whilePlaying = () => {
+    if (audioPlayer.current && progressBar.current) {
+      progressBar.current.value = audioPlayer.current.currentTime.toString()
+      animationRef.current = requestAnimationFrame(whilePlaying)
+    }
+  }
+
+  const changeRange = () => {
+    if (audioPlayer.current && progressBar.current) {
+      audioPlayer.current.currentTime = parseFloat(progressBar.current.value)
+    }
+  }
+
   const skipBackward = () => {
-    if (audioRef.current) {
-      audioRef.current.currentTime -= 10
-      setCurrentTime(audioRef.current.currentTime)
+    if (progressBar.current) {
+      progressBar.current.value = (
+        progressBar.current.valueAsNumber - 10
+      ).toString()
+      changeRange()
     }
   }
 
   const skipForward = () => {
-    if (audioRef.current) {
-      audioRef.current.currentTime += 10
-      setCurrentTime(audioRef.current.currentTime)
+    if (progressBar.current) {
+      progressBar.current.value = (
+        progressBar.current.valueAsNumber + 10
+      ).toString()
+      changeRange()
     }
   }
 
+  const formatTime = (time: number) => {
+    if (time == null) return `0:00`
+    const seconds = Math.floor(time % 60)
+    const minutes = Math.floor(time / 60)
+
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`
+  }
+
+  const totalTime = audioPlayer.current?.duration ?? 0
+
   return (
-    <div className='flex flex-col items-center justify-center gap-10 m-14'>
-      <span className='font-secondary text-4xl font-black flex flex-col items-center justify-center'>
-        {Math.floor(currentTime)}
-      </span>
+    <div className='flex flex-col items-center justify-center gap-y-5 m-14'>
+      <div className='flex gap-x-5 items-center'>
+        <span className=''>{formatTime(currentTime)}</span>
+        <input
+          type='range'
+          ref={progressBar}
+          defaultValue='0'
+          onChange={changeRange}
+          className={styles.progressBar}
+        />
+        <span>{formatTime(totalTime)}</span>
+      </div>
 
       <div className='flex gap-x-5'>
         <ButtonPlayer onClick={skipBackward}>
           <IconPlayerSkipBack stroke={2} />
         </ButtonPlayer>
-
         <ButtonPlayer onClick={togglePlay}>
           {isPlaying ? (
             <IconPlayerPause stroke={2} />
@@ -69,13 +105,11 @@ export const Player = () => {
             <IconPlayerPlay stroke={2} />
           )}
         </ButtonPlayer>
-
         <ButtonPlayer onClick={skipForward}>
           <IconPlayerSkipForward stroke={2} />
         </ButtonPlayer>
       </div>
-      <audio ref={audioRef} src='/music/temporary_music.mp3' />
-
+      <audio ref={audioPlayer} src='/music/temporary_music.mp3' />
       <p className='underline text-xl text-center'>La people</p>
     </div>
   )
--- src/components/ui/player/button-player/ButtonPlayer.tsx
+++ src/components/ui/player/button-player/ButtonPlayer.tsx
@@ -1,15 +1,14 @@
 interface Props {
-  children: React.ReactNode;
-  onClick: () => void;
+  children: React.ReactNode
+  onClick: () => void
 }
 
 export const ButtonPlayer = ({ children, onClick }: Props) => {
   return (
     <button
-      className='flex items-center justify-center bg-light rounded-full w-14 h-14 hover:bg-yellow transition-colors duration-300'
-      onClick={onClick}
-    >
+      className='flex items-center justify-center bg-light rounded-full w-10 h-10 hover:bg-yellow transition-colors duration-300'
+      onClick={onClick}>
       {children}
     </button>
-  );
-};
+  )
+}
