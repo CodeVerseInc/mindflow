fb8765c87247d35f46a4ac7480396728d8b6c038
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-18 12:04:36 -0600

    fix(sidebar): remove ';' after <Player /> component

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,15 +1,15 @@
-import { Player, GridBooks } from '@/components/ui'
-import { getSongs } from '@/lib/utils'
+import { Player, GridBooks } from '@/components/ui';
+import { getSongs } from '@/lib/utils';
 
 export const Sidebar = async () => {
-  const songs = await getSongs()
-  const randomIndex = Math.floor(Math.random() * songs.length)
+  const songs = await getSongs();
+  const randomIndex = Math.floor(Math.random() * songs.length);
 
   return (
     <>
       <Player songs={songs} initialSongIndex={randomIndex} />
-      <aside className='[grid-area:sidebar] bg-secundary rounded-xl h-full'>
-        <h2 className='text-center text-2xl font-semibold p-5 text-green'>
+      <aside className='[grid-area:sidebar] bg-secundary rounded-2xl p-5 scroll-smooth overflow-y-auto h-96 md:h-full mb-5'>
+        <h2 className='text-center text-2xl font-semibold px-5 mb-5 text-green'>
           Libros recomendados
         </h2>
         <div className='px-5 scroll-smooth overflow-y-auto h-[600px]'>
@@ -17,5 +17,5 @@ export const Sidebar = async () => {
         </div>
       </aside>
     </>
-  )
-}
+  );
+};
