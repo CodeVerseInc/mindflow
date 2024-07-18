78df274b1777c1ed400ed3521f481d2e84de68a8
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-07-18 11:45:13 -0600

    feat: remove position books

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -7,12 +7,14 @@ export const Sidebar = async () => {
 
   return (
     <>
-      <Player songs={songs} initialSongIndex={randomIndex} />;
-      <aside className='[grid-area:sidebar] bg-secundary rounded-2xl p-5 scroll-smooth overflow-y-auto h-96 md:h-full mb-5'>
-        <h2 className='text-center text-2xl font-semibold px-5 mb-5 text-green'>
+      <Player songs={songs} initialSongIndex={randomIndex} />
+      <aside className='[grid-area:sidebar] bg-secundary rounded-xl h-full'>
+        <h2 className='text-center text-2xl font-semibold p-5 text-green'>
           Libros recomendados
         </h2>
-        <GridBooks />
+        <div className='px-5 scroll-smooth overflow-y-auto h-[600px]'>
+          <GridBooks />
+        </div>
       </aside>
     </>
   )
