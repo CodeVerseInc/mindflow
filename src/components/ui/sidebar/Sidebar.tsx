688321e99d92cde39f48e7657343c34f002d79e7
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-07-18 12:25:55 -0600

    fix: remove scrollbar

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ export const Sidebar = async () => {
   return (
     <>
       <Player songs={songs} initialSongIndex={randomIndex} />
-      <aside className='[grid-area:sidebar] bg-secundary rounded-2xl p-5 scroll-smooth overflow-y-auto h-96 md:h-full mb-5'>
+      <aside className='[grid-area:sidebar] bg-secundary rounded-2xl p-5 h-full mb-5'>
         <h2 className='text-center text-2xl font-semibold px-5 mb-5 text-green'>
           Libros recomendados
         </h2>
