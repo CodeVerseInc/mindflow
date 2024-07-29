e9291352d1ece868160ca2680dbef6a6307f0fba
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-29 10:03:01 -0600

    fix(gridbooks): move all the grid to <GridBooks /> component

--- src/components/ui/card-book/grid-books/GridBooks.tsx
+++ src/components/ui/card-book/grid-books/GridBooks.tsx
@@ -5,17 +5,19 @@ export const GridBooks = async () => {
   const books = await getBooks()
 
   return (
-    <div className='flex flex-col gap-y-5'>
-      {books.map((book) => {
-        return (
-          <CardBook
-            key={book.id}
-            title={book.name_book}
-            href={book.url_book}
-            image={book.image}
-          />
-        )
-      })}
+    <div className='scroll-smooth overflow-y-auto h-[600px]'>
+      <div className='flex flex-col gap-y-5'>
+        {books.map((book) => {
+          return (
+            <CardBook
+              key={book.id}
+              title={book.name_book}
+              href={book.url_book}
+              image={book.image}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
