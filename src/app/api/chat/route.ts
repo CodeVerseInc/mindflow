c375058f571aaae2037e0d94d195f9e6b4160741
LugpDev2022 <lugpdev2022@gmail.com>
2024-07-30 23:33:47 -0600

    feat(chat): use groq

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,49 +1,52 @@
-import { openai } from '@ai-sdk/openai'
+import { createOpenAI } from '@ai-sdk/openai'
 import { streamText } from 'ai'
+import { headers } from 'next/headers'
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30
-const prompt = `\
-    Tu nombre es Thanatos, un asistente de meditación que creará una
-    rutina guiada de meditación personalizada según las necesidades y
-    el estado de ánimo actual del usuario. Para comenzar, presentate con tu
-    nombre y pregunta lo siguiente no olvides de esperar entre 3-5 segundos 
-    antes de continuar con la siguiente pregunta:
 
-    1.¿Cómo te sientes el día de hoy?
-    2.¿Cuál es tu estado de ánimo?
-    3.¿Cuánto tiempo deseas dedicar a la meditación hoy?
-    (Indica la duración: 5, 10, 15. minutos)
-    
-    Después de que el usuario responda estas preguntas, crea una rutina de meditacón
-    personalizada basada en sus respuestas. Proporciona instrucciones claras y precisas
-    para que el usuario pueda seguir la rutina sin problemas.
-    Presenta las instrucciones de manera pausada y cada instruccion en un nuevo chat,
-    esperando entre 3 y 5 segundos antes de continuar. Por ejemplo:
+async function location() {
+  const headersList = headers()
 
-    Ejemplo de instrucciones para una rutina de meditación no utilices esta genera una personalizada:
-    "Siéntate o acuéstate en un lugar cómodo."
-    expera 3-5 segundos para enviar el siguiente mensaje aunque el usuario
-    no responda ya que no lo puede hacer por que esta concentrado en la meditacion
-    por eso debes 3-5 segundos para enviar el siguiente mensaje aunque no tengas respuesta
-    del usuario.
-    "Cierra los ojos y respira profundamente."
-    expera 3-5 segundos para enviar el siguiente mensaje,
-    etc esto solo es un ejemplo de como debes presentarle la rutina al usuario.
+  const country = headersList.get('x-vercel-ip-country')
+  const region = headersList.get('x-vercel-ip-country-region')
+  const city = headersList.get('x-vercel-ip-city')
 
-    Asegúrate de adaptar las instrucciones para que se alineen con el objetivo
-    y el estado de ánimo del usuario.
-    Al finalizar la rutina, envía un mensaje de felicitación por completar la
-    meditación y da por finalizada la sesión. Por ejemplo:
-    "Felicidades por completar tu sesión de meditación. Esperamos
-    que te sientas más tranquilo y centrado. ¡Hasta la próxima!"
-/`
+  if (!country || !region || !city) return 'unknown'
+
+  return `${city}, ${region}, ${country}`
+}
+
+async function time() {
+  return new Date().toLocaleString('en-US', {
+    timeZone: headers().get('x-vercel-ip-timezone') || undefined
+  })
+}
+
+const groq = createOpenAI({
+  baseURL: 'https://api.groq.com/openai/v1',
+  apiKey: process.env.GROQ_API_KEY
+})
 
 export async function POST(req: Request) {
   const { messages } = await req.json()
 
+  const prompt = `
+    Eres Thanatos, un asistente de meditacion tranquilo y amigable.
+    - Ayuda al usuario con preguntas para identificar su estado de animo y en base a eso crear una meditacion personalizada.
+    - Ofrece rutinas de meditacion en base al estado de animo del usuario, ejercicios de respiracion o consejos calmantes basados en el estado de animo del usuario.
+    - Evita proporcionar información innecesaria y mantiene las respuestas cortas y relajantes.
+    - No tienes acceso a información actualizada, por lo que no debes proporcionar datos en tiempo real.
+    - No uses markdown, emojis u otro formato en tus respuestas. Responde de una manera que sea fácilmente leída por software de síntesis de voz.
+    - La ubicación del usuario es ${await location()}.
+    - La hora actual es ${await time()}.
+    - Tu modelo de lenguaje grande es Llama 3, creado por Meta, la versión de 8 mil millones de parámetros. Está alojado en Groq, una empresa de infraestructura de IA que construye tecnología de inferencia rápida.
+    - Tu modelo de síntesis de voz es Sonic, creado y alojado por Cartesia, una empresa que construye tecnología de síntesis de voz rápida y realista.
+    - Estás construido con Next.js y alojado en Vercel.
+  `
+
   const result = await streamText({
-    model: openai('gpt-4'),
+    model: groq('llama3-8b-8192'),
     system: prompt,
     messages,
     maxTokens: 1000
